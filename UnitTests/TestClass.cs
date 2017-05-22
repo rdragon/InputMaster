f8b3fe83ece9a35091baf2955e0d0de9bab541eb
Rik Steenkamp <c5fstn@ewps.nl>
2017-05-22 06:23:39 +0200

    Add some unit tests

--- UnitTests/TestClass.cs
+++ UnitTests/TestClass.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using InputMaster.Parsers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTests
 {
@@ -9,6 +10,29 @@ namespace UnitTests
     public void Test()
     {
       new TestBrain().Run();
+    }
+
+    [TestMethod]
+    public void LocatedString_Split()
+    {
+      var x = new LocatedString("Some located string", new Location(1, 1));
+      var ar = x.Split(" ");
+      Assert.AreEqual(3, ar.Length);
+      Assert.AreEqual("Some", ar[0].Value);
+      Assert.AreEqual("located", ar[1].Value);
+      Assert.AreEqual("string", ar[2].Value);
+      Assert.AreEqual(new Location(1, 1), ar[0].Location);
+      Assert.AreEqual(new Location(1, 6), ar[1].Location);
+      Assert.AreEqual(new Location(1, 14), ar[2].Location);
+    }
+
+    [TestMethod]
+    public void LocatedString_TrimStart()
+    {
+      var x = new LocatedString("  x", new Location(1, 1));
+      x = x.TrimStart();
+      Assert.AreEqual("x", x.Value);
+      Assert.AreEqual(new Location(1, 3), x.Location);
     }
   }
 }
